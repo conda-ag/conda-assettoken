4ee563dd28635c3445025c3e2cf0d6b178605ff5
CodingYourLife <johannes.mayer@conda.at>
2018-06-20 13:59:37 +0200

    CRWDAssetToken generator comments

--- contracts/CRWDAssetToken.sol
+++ contracts/CRWDAssetToken.sol
@@ -13,24 +13,43 @@ contract CRWDAssetToken is DividendAssetToken {
 
     address public clearingAddress;
 
+    /** @dev ERC20 transfer function overlay to transfer tokens and do clearing.
+      * @param _to The recipient address.
+      * @param _amount The amount.
+      * @return A boolean that indicates if the operation was successful.
+      */
     function transfer(address _to, uint256 _amount) public returns (bool success) {
         uint256 transferValue = _amount.mul(baseRate).div(1000);
         ICRWDClearing(clearingAddress).clearFunds(baseCurrency, msg.sender, _to, transferValue);
         return super.transfer(_to, _amount);
     }
 
+    /** @dev ERC20 transferFrom function overlay to transfer tokens and do clearing.
+      * @param _from The sender address (requires approval).
+      * @param _to The recipient address.
+      * @param _amount The amount.
+      * @return A boolean that indicates if the operation was successful.
+      */
     function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
         uint256 transferValue = _amount.mul(baseRate).div(1000);
         ICRWDClearing(clearingAddress).clearFunds(baseCurrency, _from, _to, transferValue);
         return super.transferFrom(_from, _to, _amount);
     }
 
+    /** @dev Mint function overlay to mint/create tokens and do clearing.
+      * @param _to The address that will receive the minted tokens.
+      * @param _amount The amount of tokens to mint.
+      * @return A boolean that indicates if the operation was successful.
+      */
     function mint(address _to, uint256 _amount) public onlyOwner canMint returns (bool) {
         uint256 transferValue = _amount.mul(baseRate).div(1000);
         ICRWDClearing(clearingAddress).clearFunds(baseCurrency, _to, _to, transferValue);
         return super.mint(_to,_amount);
     }
 
+    /** @dev Set clearing address that receives clearing.
+      * @param _clearingAddress Address to be used for clearing.
+      */
     function setClearingAddress(address _clearingAddress) public {
         clearingAddress = _clearingAddress;
     }
