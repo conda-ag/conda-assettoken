a4d4ed5627fea0e6020e946695a475450e3ffb6e
CodingYourLife <johannes.mayer@conda.at>
2018-06-22 15:01:23 +0200

    ERC20TestTokenRetFalse for testing wrongly implemented ERC20 contracts returning a boolean instead of reverting

---
+++ contracts/test/ERC20TestTokenRetFalse.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.23;
+
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+
+/** @title Sample MintableToken for tests. */
+contract ERC20TestTokenRetFalse is MintableToken {
+    //Investment Details
+    string public name = "Broken Token";
+    string public symbol = "BRK";
+    uint256 public decimals = 18;
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        return false;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        return false;
+    }
+}
