380fc323175d8f4e2572b302085da67b8e3c44f0
CodingYourLife <johannes.mayer@conda.at>
2018-08-30 17:52:47 +0200

    interface method split up

--- contracts/test/MockCRWDClearing.sol
+++ contracts/test/MockCRWDClearing.sol
@@ -6,7 +6,7 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../interfaces/ICRWDClearing.sol";
 
 /** @title mocked clearing for tests. */
-contract MockCrwdClearing is Ownable {
+contract MockCrwdClearing is Ownable, ICRWDClearing {
     using SafeMath for uint256;
 
     uint256 companyFee;
@@ -16,6 +16,24 @@ contract MockCrwdClearing is Ownable {
     address companyAddress;
 
     function clearFunds(address /*_underlyingCurrency*/, address _from, address /*_to*/, uint256 _amount) public returns (bool) {
+        
+
+        if (companyFee > 0) {
+            uint256 feeCompanyToPay = _amount.mul(companyFee).div(1000);
+            if (!ERC20(crwdToken).transferFrom(companyAddress, condaAddress, feeCompanyToPay)) 
+                revert();
+        }
+
+        if (investorFee > 0) {
+            uint256 feeInvestorToPay = _amount.mul(investorFee).div(1000);
+            if (!ERC20(crwdToken).transferFrom(_from, condaAddress, feeInvestorToPay)) 
+                revert();
+        }
+
+        return true;
+    }
+
+    function clearMintFunds(address /*_underlyingCurrency*/, address _from, address /*_to*/, uint256 _amount) public returns (bool) {
         
 
         if (companyFee > 0) {
