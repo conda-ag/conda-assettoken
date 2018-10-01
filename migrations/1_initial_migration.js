ca229a66612890ec89b94efe0fb9cda5a7aea8f3
CodingYourLife <johannes.mayer@conda.at>
2018-10-01 19:48:27 +0200

    async await support in migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = async (deployer) => {
-  await deployer.deploy(Migrations)
+module.exports = (deployer) => {
+  deployer.then(async () => { //fake async await support: https://github.com/trufflesuite/truffle/issues/501
+    await deployer.deploy(Migrations)
+  })
 }
--- migrations/3_deploy_AssetToken1.js
+++ migrations/3_deploy_AssetToken1.js
@@ -40,57 +40,59 @@ const updateDeploymentConfig = (selectiveUpdateObject) => {
     fs.writeFileSync(configFilename, JSON.stringify(jsonConfig, null, 2), 'utf8')
 }
 
-module.exports = async (deployer, network, accounts) => {
+module.exports = (deployer, network, accounts) => {
+    deployer.then(async () => { //fake async await support: https://github.com/trufflesuite/truffle/issues/501
 
-    if (accounts[1] === undefined || accounts[1] == null) {
-        console.log("accounts[1]: " + accounts[1])
-        throw new Error('HDWalletProvider needs to specify num_addresses (default is 1)')
-    }
+        if (accounts[1] === undefined || accounts[1] == null) {
+            console.log("accounts[1]: " + accounts[1])
+            throw new Error('HDWalletProvider needs to specify num_addresses (default is 1)')
+        }
 
-    // deploy libraries
-    await deployer.deploy(AssetTokenL)
+        // deploy libraries
+        await deployer.deploy(AssetTokenL)
 
-    //link libraries
-    await deployer.link(AssetTokenL, BasicAssetToken)
-    await deployer.link(AssetTokenL, DividendAssetToken)
-    await deployer.link(AssetTokenL, CRWDAssetToken)
-    await deployer.link(AssetTokenL, EquityAssetToken)
-    await deployer.link(AssetTokenL, FeatureCapitalControl)
-    await deployer.link(AssetTokenL, DividendEquityAssetToken)
-    await deployer.link(AssetTokenL, FeatureCapitalControlWithForcedTransferFrom)
+        //link libraries
+        await deployer.link(AssetTokenL, BasicAssetToken)
+        await deployer.link(AssetTokenL, DividendAssetToken)
+        await deployer.link(AssetTokenL, CRWDAssetToken)
+        await deployer.link(AssetTokenL, EquityAssetToken)
+        await deployer.link(AssetTokenL, FeatureCapitalControl)
+        await deployer.link(AssetTokenL, DividendEquityAssetToken)
+        await deployer.link(AssetTokenL, FeatureCapitalControlWithForcedTransferFrom)
 
-    //deploy contracts
-    // deployer.deploy(MockCRWDClearing)
+        //deploy contracts
+        // deployer.deploy(MockCRWDClearing)
 
-    const owner = accounts[0]
-    const capitalControl = accounts[1]
-    const pauseControl = accounts[2]
-    const tokenRescueControl = accounts[3]
+        const owner = accounts[0]
+        const capitalControl = accounts[1]
+        const pauseControl = accounts[2]
+        const tokenRescueControl = accounts[3]
 
-    console.log("OWNER:::::"+owner)
-    console.log("CAPITALCONTROL:::::"+capitalControl)
-    console.log("PAUSECONTROL:::::"+pauseControl)
-    console.log("TOKENRESCUECONTROL:::::"+tokenRescueControl)
+        console.log("OWNER:::::"+owner)
+        console.log("CAPITALCONTROL:::::"+capitalControl)
+        console.log("PAUSECONTROL:::::"+pauseControl)
+        console.log("TOKENRESCUECONTROL:::::"+tokenRescueControl)
 
-    await deployer.deploy(DividendEquityAssetToken, capitalControl, {from: owner})
-    const token = await DividendEquityAssetToken.at(DividendEquityAssetToken.address)
+        await deployer.deploy(DividendEquityAssetToken, capitalControl, {from: owner})
+        const token = await DividendEquityAssetToken.at(DividendEquityAssetToken.address)
 
-    await token.setMetaData("CONDA AG Equity Token", "CONDA", {from: owner})
+        await token.setMetaData("CONDA AG Equity Token", "CONDA", {from: owner})
 
-    const deploymentConfig = getDeploymentJsonConfig()
-    let clearingAddress = ZERO_ADDRESS
-    if(deploymentConfig.hasOwnProperty('clearingAddress')) {
-        clearingAddress = deploymentConfig.clearingAddress
-    }
+        const deploymentConfig = getDeploymentJsonConfig()
+        let clearingAddress = ZERO_ADDRESS
+        if(deploymentConfig.hasOwnProperty('clearingAddress')) {
+            clearingAddress = deploymentConfig.clearingAddress
+        }
 
-    console.log("=> Used clearing address: " + clearingAddress)
+        console.log("=> Used clearing address: " + clearingAddress)
 
-    await token.setClearingAddress(clearingAddress, {from: owner})
+        await token.setClearingAddress(clearingAddress, {from: owner})
 
-    await token.setRoles(pauseControl, tokenRescueControl, {from: owner})
+        await token.setRoles(pauseControl, tokenRescueControl, {from: owner})
 
-    // await token.finishMinting({from: owner})
-    // await token.setTokenAlive({from: owner})
+        // await token.finishMinting({from: owner})
+        // await token.setTokenAlive({from: owner})
 
-    updateDeploymentConfig({ assetToken1: DividendEquityAssetToken.address })
+        updateDeploymentConfig({ assetToken1: DividendEquityAssetToken.address })
+    })
 }
